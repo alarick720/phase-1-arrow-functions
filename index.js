1678ff376ad85862f72b2ca2beba92c3c0c8bad8
Alarick Whitted <awhitted834@gmail.com>
2023-12-30 12:47:01 -0500

    Done

---
+++ index.js
@@ -0,0 +1,21 @@
+function divide(){
+    const result = 2000 / 100;
+    return 20;
+}
+
+const add= (a, b) => (a + b);
+    let a = 3;
+    let b = 4;
+    const result = add(3, 4);
+    console.log(result); // output: 7, because 3 + 4 is 7
+
+    const square = (number) => number * number;
+    console.log(square(0)); // output: 0, because 0 squared is 0
+
+  
+  
+
+
+
+
+
